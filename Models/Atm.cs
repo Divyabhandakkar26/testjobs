5ec1a0b8e8e3bfc206a494c1f1f1ecfa7e8e9b9a
Divyabhandakkar26 <148749077+Divyabhandakkar26@users.noreply.github.com>
2023-10-23 13:35:05 +0530

    atm
    test atm

---
+++ Models/Atm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Atm
+    {
+        public int AtmId { get; set; }
+
+        public int MyProperty { get; set; }
+    }
+}
